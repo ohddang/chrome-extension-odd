ac6eed72e59ba3ded8988c1ceb184b0e1d13425e
ohddang <ohddang509@gmail.com>
2024-02-27 21:47:34 +0900

    setting: 환경세팅 중 변경사항 저장

--- src/background/background.ts
+++ src/background/background.ts
@@ -1,20 +1,21 @@
 //This seems to force the content-script to be reloaded when the history changes
-chrome.webNavigation.onHistoryStateUpdated.addListener(async (details) => {
+chrome.webNavigation.onHistoryStateUpdated.addListener(async () => {
   const getCurrentTab = async () => {
-    let queryOptions = { active: true, lastFocusedWindow: true };
-    let [tab] = await chrome.tabs.query(queryOptions);
+    const queryOptions = { active: true, lastFocusedWindow: true };
+    const [tab] = await chrome.tabs.query(queryOptions);
     return tab;
   };
 
-  let tab: any = await getCurrentTab();
+  const tab: chrome.tabs.Tab = await getCurrentTab();
 
-  let url = new URL(tab.url);
+  const url = new URL(tab.url || "");
   if (url.hostname !== "/your website here/") {
+    //replace with your website
     return;
   }
 
   chrome.scripting.executeScript({
-    target: { tabId: tab.id },
+    target: { tabId: tab.id || 0 },
     func: () => {}, //this literally will execute an empty function in the current tab which seems to be enough to reload or reconnect the content-script
   });
 });
